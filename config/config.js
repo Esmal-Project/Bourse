bfc3e429e89d2da875750a7c759a59c2028c98cb
Esmal-Project <yasharesmaeili@gmail.com>
2026-09-20 12:38:38 -0700

    Prepare market data routes

--- config/config.js
+++ config/config.js
@@ -1,1 +1,8 @@
-window.BOURSE_CONFIG={worker:"https://brsapi-proxy.yasharesmaeili.workers.dev",historyPath:"/tsetmc/history"};
+window.BOURSE_CONFIG={
+  worker:"https://brsapi-proxy.yasharesmaeili.workers.dev",
+  historyPath:"/tsetmc/history",
+  marketWatchPath:null,
+  quotePath:null,
+  orderbookPath:null,
+  clientTypePath:null
+};
